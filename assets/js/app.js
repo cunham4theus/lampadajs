fd80ed0620a17cb55c73eb0844bfe6fc21e6f77e
cunham4theus <cunhamatheus.m4@gmail.com>
2022-12-26 02:37:47 -0300

    broken lamp

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,25 @@ const turnOn = document.getElementById('turn-on');
 const turnOff = document.getElementById('turn-off');
 const lamp = document.getElementById('lamp');
 
+
+function isLampBroken() {
+  return lamp.src.indexOf('quebrada') > -1
+}
 function lampOn() {
-  lamp.src = '/assets/img/acesa.png';
+  if(!isLampBroken()) {
+    lamp.src = '/assets/img/acesa.png';
+  }
 }
-turnOn.addEventListener('click', lampOn);
-
 function lampOff() {
-  lamp.src = '/assets/img/apagada.png';
+  if(!isLampBroken()) {
+    lamp.src = '/assets/img/apagada.png';
+  }
 }
-turnOff.addEventListener('click', lampOff);
-
 function lampBroken() {
   lamp.src = '/assets/img/quebrada.png';
 }
-
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
 lamp.addEventListener('mouseover', lampOn);
 lamp.addEventListener('mouseleave', lampOff);
 lamp.addEventListener('dblclick', lampBroken);
